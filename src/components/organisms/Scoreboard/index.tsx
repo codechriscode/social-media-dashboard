d8c54a0c72167ecb5f81cc0a3acafc529029e6a6
Chris Sousa <chrisqcs@tuta.io>
2022-03-12 02:17:12 -0300

    Scoreboard/Dashboard refactor

--- src/components/organisms/Scoreboard/index.tsx
+++ src/components/organisms/Scoreboard/index.tsx
@@ -1,16 +1,62 @@
 import MainCard from "../../molecules/Cards/MainCard";
-import mock from "../../../mock/payload.json";
+import mock from "../../../mock/payload";
 
-export default function Overview() {
+import "./styles.css";
+import React, { useState } from "react";
+
+const handleDragStart = (e: React.DragEvent<HTMLSpanElement>) => {
+  const mediumName = getMediumFromCard(e.currentTarget);
+  e.dataTransfer.setData("dragged", mediumName);
+};
+
+const allowDrop = (e: React.DragEvent<HTMLSpanElement>) => {
+  e.preventDefault();
+};
+
+const getMediumFromCard = (card: HTMLElement) => {
+  return card.classList[card.classList.length-1];
+};
+
+export default function Scoreboard() {
+  const [media, _] = useState(mock.media);
+
+  const getPositions = () => {
+    let lsPos = localStorage.getItem("mainCardPositions");
+    if (lsPos) return JSON.parse(lsPos) as Array<string>;
+    return Object.keys(media);
+  };
+
+  const [positions, setPositions] = useState(getPositions());
+
+  const finishMove = (e: React.DragEvent<HTMLElement>) => {
+    const beingDropped = e.dataTransfer.getData("dragged");
+    const droppedOver = getMediumFromCard(e.currentTarget);
+    const newIndex = positions.findIndex((medium) => medium === droppedOver);
+    const oldIndex = positions.findIndex((medium) => medium === beingDropped);
+
+    let newPositions = [...positions];
+    newPositions.splice(oldIndex, 1);
+    newPositions.splice(newIndex, 0, beingDropped);
+    // update state and localStorage
+    setPositions(newPositions);
+    localStorage.setItem("mainCardPositions", JSON.stringify(newPositions));
+  };
 
   return (
-    <div className="overview">
-      {mock.media.map((medium) => {
+    <div className="grid-container">
+      {positions.map((mediumName) => {
+        const medium = media[mediumName];
+        const identifier = `${medium.name}.${medium.username}.Main`;
         return (
           <MainCard
+            draggable
+            ondragstart={handleDragStart}
+            ondragover={allowDrop}
+            ondrop={finishMove}
             name={medium.name}
             status={medium.status}
             username={medium.username}
+            key={identifier}
           />
         );
       })}
--- src/components/template/Dashboard/index.tsx
+++ src/components/template/Dashboard/index.tsx
@@ -2,6 +2,7 @@ import Toggle from "../../atoms/Toggle";
 import Typography from "../../atoms/Typography";
 import Scoreboard from "../../organisms/Scoreboard";
 import TopPanel from "../../molecules/TopPanel";
+import MiniCard from "../../molecules/Cards/MiniCard";
 
 export default function Dashboard() {
 
@@ -9,8 +10,12 @@ export default function Dashboard() {
     <>
       <TopPanel />
       <Scoreboard />
+      {/* <MiniCard 
+        name="youtube"
+        other_status={{unit: "Total Views", value: 500, change_pc: 25}}
+        username="Nathan"
+      /> */}
       <Typography variant="title2">Overview - Today</Typography>
-      <Toggle />
     </>
   );
 }
