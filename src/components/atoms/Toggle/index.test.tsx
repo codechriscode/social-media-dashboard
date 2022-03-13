be05fef51f6972848fe9c681c83d8dc3c5f34909
Chris Sousa <chrisqcs@tuta.io>
2022-03-12 22:40:25 -0300

    test:Toggle

---
+++ src/components/atoms/Toggle/index.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Toggle from ".";
+
+test("onChange gets fired", () => {
+  const toggleFn = jest.fn();
+  render(<Toggle onchange={toggleFn} />);
+  const toggle = screen.getByRole("checkbox");
+  expect(toggle).toBeInTheDocument();
+  
+  fireEvent.click(toggle);
+  expect(toggle).toBeChecked();
+  expect(toggleFn).toHaveBeenCalled();
+
+  fireEvent.click(toggle);
+  expect(toggle).not.toBeChecked();
+  expect(toggleFn).toHaveBeenCalledTimes(2);
+});
