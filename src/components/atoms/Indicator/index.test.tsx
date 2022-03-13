9794016e01ca1d200b98a2103696e264083ebfa6
Chris Sousa <chrisqcs@tuta.io>
2022-03-12 21:56:53 -0300

    test:Indicator

---
+++ src/components/atoms/Indicator/index.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom";
+import Indicator from ".";
+
+describe("Indicator", () => {
+  test("Has optional items if requested", () => {
+    render(<Indicator value={50} percent arrow period={3} />);
+    expect(screen.getByAltText("Green up arrow")).toBeInTheDocument();
+    expect(screen.getByText("Last 3 days", {exact: false})).toBeInTheDocument();
+    expect(screen.getByText("50%", {exact: false})).toBeInTheDocument();
+  });
+});
--- src/components/atoms/Indicator/index.tsx
+++ src/components/atoms/Indicator/index.tsx
@@ -1,22 +1,22 @@
 import Typography from "../Typography";
 import Icon from "../Icon";
 
-import { getIconName, calcPeriod, getColor } from "../../../helpers";
+import { getArrowName, calcPeriod, getColor } from "../../../helpers";
 import Box from "../Box";
 
 export default function Indicator(props: {
   value: number;
   arrow?: boolean;
-  period?: boolean;
+  period?: number | boolean;
   percent?: boolean;
 }) {
   const { value, arrow, period = false, percent = false } = props;
   return (
     <Box boxClass="center-row">
-      {value && arrow ? <Icon iconName={getIconName(value)} /> : ""}
+      {value && arrow ? <Icon iconName={getArrowName(value)} /> : ""}
       <Typography variant="subtitle" style={{ color: `var(${getColor(value)})` }}>
         {`${Math.abs(value)}${percent ? "%" : ""} ${
-          period ? calcPeriod(0) : ""
+          period ? calcPeriod(period as number) : ""
         }`}
       </Typography>
     </Box>
