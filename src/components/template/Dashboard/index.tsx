beafcf7e265a3694a22873f13bc3a6b3ffcd2c28
Chris Sousa <chrisqcs@tuta.io>
2022-08-20 20:19:52 -0300

    Dashboard using Context

--- src/components/template/Dashboard/index.tsx
+++ src/components/template/Dashboard/index.tsx
@@ -1,126 +1,40 @@
-import { useCallback, useState } from "react";
-import { mock, PayloadType, MediaType } from "../../../store/payload";
-import {
-  socialNetworks,
-  SocialNetworkNames,
-} from "../../../store/socialNetworks";
+import { useContext } from "react";
 
 import Scoreboard from "../../organisms/Scoreboard";
 import TopPanel from "../../molecules/TopPanel";
 import Overview from "../../organisms/Overview";
-import FloatingMenu, { FloatingMenuProps } from "../../organisms/FloatingMenu";
-import Card from "../../atoms/Card";
+import FloatingMenu from "../../organisms/FloatingMenu";
 import Overlay from "../../atoms/Overlay";
-
-const emptyState: PayloadType = {
-  period: 0,
-  total_followers: 0,
-  media: {},
-};
-
-const recoverState = () => {
-  const state = localStorage.getItem("state");
-  if (state) {
-    return JSON.parse(state);
-  } else {
-    return { mock };
-    // TODO return emptyState when "add first" feature is ready
-  }
-};
-
-const generateNew = (networkName: SocialNetworkNames, username: string) => {
-  const networkParams = socialNetworks[networkName];
-  const cnxID = `${Math.trunc(Math.random() * 10000000000)}`;
-  const newEntry = {
-    [cnxID]: {
-      socialNetwork: networkName,
-      username: username,
-      status: {
-        value: Math.trunc(Math.random() * 1500000),
-        unit: networkParams.metrics.main,
-        change: Math.trunc(Math.random() * 150),
-      },
-      other_status: [
-        {
-          unit: networkParams.metrics.other[0],
-          value: Math.trunc(Math.random() * 15000),
-          change_pc: Math.trunc(Math.random() * 150),
-        },
-        {
-          unit: networkParams.metrics.other[1],
-          value: Math.trunc(Math.random() * 15000),
-          change_pc: Math.trunc(Math.random() * 150),
-        },
-      ],
-    },
-  };
-  return newEntry;
-};
-
-const menuButtons: FloatingMenuProps["buttons"] = [
-  "Twitter",
-  "Instagram",
-  "Facebook",
-  "YouTube",
-  "delete",
-];
+import AppContext from "../../../store/AppContext";
+import Typography from "../../atoms/Typography";
+import Icon from "../../atoms/Icon";
+import Box from "../../atoms/Box";
 
 export default function Dashboard() {
-  const [data, setData] = useState<PayloadType>(recoverState());
-  // TODO update both data and localstorage on
+  const ctx = useContext(AppContext);
 
-  const [overlay, setOverlay] = useState<boolean>(false);
-  const setOverlayCallback = (active: boolean) => {
-    setOverlay(active);
-  };
-
-  const addNew = (networkName: SocialNetworkNames, username: string) => {
-    const newEntry = generateNew(networkName, username);
-    const newTotal = data.total_followers + newEntry[0].status.value;
-    setData(prevState => ({
-      ...prevState,
-      total_followers: newTotal,
-      media: { ...prevState.media, ...newEntry },
-    }));
-  };
-
-  const remove = (cnxID: string) => {
-    let newData = Object.assign({}, data);
-    let toDelete = (newData.media as MediaType)[cnxID];
-    let newTotal = newData.total_followers - toDelete.status.value;
-    newData = { ...newData, total_followers: newTotal };
-
-    delete (newData.media as MediaType)[cnxID];
-    setData(newData);
-  };
-
-  const removeAll = () => {
-    setData(emptyState);
-  };
-
-  const callbacks = {
-    addNew: addNew,
-    // update: update,
-    removeAll: removeAll,
-    remove: remove,
-    setOverlay: setOverlayCallback,
-  };
+  const menuButtons = [...Object.keys(ctx.socialNetworks), "delete"];
 
   return (
     <>
-      <TopPanel totalFollowers={data.total_followers || 0} />
-      {data.media && Object.keys(data.media).length ? (
-        <>
-          <Scoreboard media={data.media} period={data.period} />
-          <Overview media={data.media} period={data.period} />
-        </>
-      ) : (
-        <Card cardClass="main">Add new</Card>
-      )}
-      <FloatingMenu buttons={menuButtons} callbacks={callbacks} />
+      <TopPanel totalFollowers={ctx.profiles.state.total_followers || 0} />
+      <main>
+        {ctx.profiles.state.media && Object.keys(ctx.profiles.state.media).length ? (
+          <>
+            <Scoreboard media={ctx.profiles.state.media} period={ctx.profiles.state.period} />
+            <Overview media={ctx.profiles.state.media} period={ctx.profiles.state.period} order={ctx.profiles.positions} />
+          </>
+        ) : (
+          <Box boxClass="center-column">
+            <Icon iconName="famous" />
+            <Typography variant="title2">Click the menu to get started</Typography>
+          </Box>
+        )}
+      </main>
+      <FloatingMenu buttons={menuButtons} />
       <Overlay
         style={
-          overlay
+          ctx.overlay.state
             ? { visibility: "visible", opacity: 1 }
             : { visibility: "hidden", opacity: 0 }
         }
